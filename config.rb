71609738df80ae85eee1e1e5265b645ea9ad7adf
Ufuk Kayserilioglu <ufuk@paralaus.com>
2015-03-15 04:18:52 +0200

    Turn off asset hash for now

--- config.rb
+++ config.rb
@@ -62,7 +62,7 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  # activate :asset_hash
 
   # Use relative URLs
   activate :relative_assets
