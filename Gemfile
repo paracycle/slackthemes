1292fb455e45a37d23d5fec433f016ce58fcc11a
Ufuk Kayserilioglu <ufuk@paralaus.com>
2015-03-15 04:02:07 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
+
+gem "middleman", "~>3.3.10"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+gem "middleman-pry", "~> 0.0.4"
+gem "middleman-deploy", "~> 1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem "tzinfo-data", platforms: [:mswin, :mingw]
---
+++ source/javascripts/all.js
@@ -0,0 +1,18 @@
+//= require_tree .
+
+angular.module('theme', [])
+    .controller('ThemeController', function($scope) {
+       $scope.$watch(
+            function() { return $scope.theme; },
+            function(theme) {
+                parts = theme.split(',');
+                $scope.column_bg = parts[0];
+                $scope.menu_bg = parts[1];
+                $scope.active_item = parts[2];
+                $scope.active_item_text = parts[3];
+                $scope.hover_item = parts[4];
+                $scope.text_color = parts[5];
+                $scope.active_presence = parts[6];
+                $scope.mention_badge = parts[7];
+        });
+    });
