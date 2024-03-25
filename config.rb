86468caa1c859293148543912f9e364ca1c2206b
Ufuk Kayserilioglu <ufuk@paralaus.com>
2024-03-25 22:55:46 +0200

    Remove last remnant of ANALYTICS_ID

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ activate :deploy do |deploy|
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end if ENV['ANALYTICS_ID']
+end
 
 
 set :css_dir, 'stylesheets'
